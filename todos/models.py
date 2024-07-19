191f14de1e392cc9cf26257d9abcdd238c765e8e
eria-developer <eriddeveloper@gmail.com>
2024-07-19 19:19:58 +0300

    added the owner in models of task and category

--- todos/models.py
+++ todos/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from authentication.models import CustomUser
 
 class Category(models.Model):
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=254)
 
     def __str__(self):
@@ -9,6 +10,7 @@ class Category(models.Model):
     
 
 class Task(models.Model):
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=254, null=False)
     completed = models.BooleanField(default=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="task_category")
